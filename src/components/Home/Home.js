e95b76eed3a3f71cc2b7f250818fa25e30684108
Austine3000 <diribe3000@gmail.comgit config --global user.email diribe3000@gmail.com>
2019-03-21 09:05:54 +0100

    Removed static data for graph

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,37 +5,6 @@ import * as home from "../../store/actions";
 import { Bar } from "react-chartjs-2";
 import "./Home.scss";
 
-const data = {
-  labels: [
-    "MAR 2018",
-    "APR 2018",
-    "MAY 2018",
-    "JUN 2018",
-    "JUL 2018",
-    "AUG 2018"
-  ],
-  datasets: [
-    {
-      label: "My First dataset",
-      backgroundColor: "rgb(238, 168, 38)",
-      fillColor: "rgba(220,220,220,0.5)",
-      strokeColor: "rgba(220,220,220,0.8)",
-      highlightFill: "rgba(220,220,220,0.75)",
-      highlightStroke: "rgba(220,220,220,1)",
-      data: [667, 2059, 800, 1081, 2300, 1755]
-    },
-    {
-      label: "My Second dataset",
-      backgroundColor: "rgb(49, 49, 155)",
-      fillColor: "rgb(64, 64, 130)",
-      strokeColor: "rgb(64, 64, 130)",
-      highlightFill: "rgb(64, 64, 130)",
-      highlightStroke: "rgb(64, 64, 130)",
-      data: [2008, 1408, 1440, 1945, 1860, 2227]
-    }
-  ]
-};
-
 const options = {
   scales: {
     yAxes: [
